db7b4be35aabf363ff0042d0cf1aea38931b6075
Longle183 <lethanhlong1224@gmail.com>
2024-11-28 10:36:20 +0700

    Add files via upload

---
+++ btvn session7 06.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int num[5]={1,2,3,4,5};
+	for(int i=0;i<5;i++){
+		if(num[i]%2==0){
+			num[i]+=3;
+		}else{
+			num[i]+=2;
+		}
+	}
+	printf("Input: 1 2 3 4 5\n");
+	printf("Output: \n");
+	for(int i=0;i<5;i++){
+		printf("%d ",num[i]);
+	}
+	return 0;
+}
